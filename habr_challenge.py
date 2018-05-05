93a7f83066b2540faaeae08ab716d8586203ee74
Grin941 <labamifi@gmail.com>
2018-05-05 11:58:27 +0300

    Wrote simple ReportGenerator

--- habr_challenge.py
+++ habr_challenge.py
@@ -56,7 +56,7 @@ class SiteConfig:
         selector = self._site_config.get(selector_name)
         if selector is None:
             raise AttributeError(
-                '"{selector_name}" not defined in {site_url} config'.format(
+                '"{selector_name}" selector not defined in {site_url} config'.format(
                     site_url=self._site_config['url'],
                     selector_name=selector_name
                 )
@@ -95,7 +95,7 @@ class ParserDataCollector:
 
     def __init__(self, args):
         self._parser_result = collections.defaultdict(collections.Counter)
-        self.parser_collector = self._collect()
+        self._parser_data_collector = self._collect()
 
     def _normalize_publication_datetime(self, article_publication_datetime):
         datetime_parsed = dateparser.parse(article_publication_datetime)
@@ -112,7 +112,7 @@ class ParserDataCollector:
     def _normalize_title(self, article_title):
         morph = pymorphy2.MorphAnalyzer()
         title_words = article_title.lower().strip(
-            string.punctuation
+            string.punctuation + "«»"
         ).split(' ')
         title_morphs = (morph.parse(word)[0] for word in title_words)
         title_nouns = filter(
@@ -134,7 +134,7 @@ class ParserDataCollector:
             self._parser_result[article_week_range].update(article_title_words)
 
     def collect(self, article_data):
-        self.parser_collector.send(article_data)
+        self._parser_data_collector.send(article_data)
 
     @property
     def result(self):
@@ -206,17 +206,85 @@ class Crawler:
             )
             yield articles_list.text
 
-    def crawl(self, args):
+    def crawl(self, user_settings):
         parser = Parser(self._site_config)
-        parser_data_collector = ParserDataCollector(args)
-        show_progress = progress_bar if args.show_progress else lambda x: x
+        parser_data_collector = ParserDataCollector(user_settings)
+        show_progress = progress_bar if \
+            user_settings.show_progress else lambda x: x
         for articles_list in self._crawl_articles(
-            pages_range=show_progress(range(2, args.pages + 1))
+            pages_range=show_progress(range(1, user_settings.pages + 1))
         ):
             for article_data in parser.parse_articles(articles_list):
                 parser_data_collector.collect(article_data)
 
         return parser_data_collector.result
+
+
+class ReportGenerator:
+
+    def __init__(self, report_data_json):
+        self._report_data_json = report_data_json
+        self._report_header = (
+            'Начало недели', 'Конец недели', 'Популярные слова'
+        )
+
+        self._report_columns_width = self._get_report_columns_width()
+        self._table_width = sum(
+            self._report_columns_width
+        ) + 3  # Keep in mind 3 column separators
+
+        self._report_format_string = (
+            '{week_begin:{week_begin_col_width}}{sep}'
+            '{week_end:^{week_end_col_width}}{sep}'
+            '{popular_words:^{popular_words_col_width}}{sep}'
+        )
+        self._columns_separator = '|'
+        self._rows_separator = '-'
+
+    def _get_report_columns_width(self):
+        week_begin_col_width = len(self._report_header[0]) + 1
+        week_end_col_width = len(self._report_header[1]) + 2
+        popular_words_col_width = len(
+            max(self._report_data_json.values(), key=len)
+        ) + 2
+
+        return (
+            week_begin_col_width,
+            week_end_col_width,
+            popular_words_col_width
+        )
+
+    def _print_header(self):
+        header = self._report_format_string.format(
+            week_begin=self._report_header[0],
+            week_begin_col_width=self._report_columns_width[0],
+            week_end=self._report_header[1],
+            week_end_col_width=self._report_columns_width[1],
+            popular_words=self._report_header[2],
+            popular_words_col_width=self._report_columns_width[2],
+            sep=self._columns_separator
+        )
+        print(self._rows_separator * self._table_width)
+        print(header)
+        print(self._rows_separator * self._table_width)
+
+    def _print_body(self):
+        for week, popular_words in self._report_data_json.items():
+            body = self._report_format_string.format(
+                week_begin=week[0],
+                week_begin_col_width=self._report_columns_width[0],
+                week_end=week[1],
+                week_end_col_width=self._report_columns_width[1],
+                popular_words=popular_words,
+                popular_words_col_width=self._report_columns_width[2],
+                sep=self._columns_separator
+            )
+            print(body)
+        print(self._rows_separator * self._table_width)
+
+    def print_report(self):
+        self._print_header()
+        self._print_body()
 
 
 if __name__ == '__main__':
@@ -229,11 +297,11 @@ if __name__ == '__main__':
                 (default: %(default)s)?"""
     )
     parser.add_argument(
-        '--show-progress', action='store_true',
+        '--show-progress', action='store_false',
         help='do you want to see progress bar?'
     )
-    args = parser.parse_args()
-    assert args.pages > 0, "Please pass --pages > 10"
+    user_settings = parser.parse_args()
+    assert user_settings.pages > 0, "Please pass --pages > 10"
 
-    articles_data = Crawler('habr').crawl(args)
-    print(articles_data)
+    articles_data = Crawler('habr').crawl(user_settings)
+    ReportGenerator(articles_data).print_report()
