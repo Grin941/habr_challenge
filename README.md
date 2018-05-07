0cafe09058e849ec26a5684593d0503486936d4b
Александр Гриненко <labamifi@gmail.com>
2018-05-07 16:27:27 +0300

    Make project more user friendly

--- README.md
+++ README.md
@@ -2,8 +2,11 @@ habr_challenge
 ==============
 [![Build Status](https://travis-ci.org/Grin941/habr_challenge.svg?branch=master)](https://travis-ci.org/Grin941/habr_challenge)
 [![Coverage by codecov.io](https://codecov.io/gh/Grin941/habr_challenge/branch/master/graphs/badge.svg?branch=master)](https://codecov.io/gh/Grin941/habr_challenge?branch=master)
+[![Maintainability](https://api.codeclimate.com/v1/badges/925e35d9ee96feddf59b/maintainability)](https://codeclimate.com/github/Grin941/habr_challenge/maintainability)
 
 Print 3 most popular nouns from Habr feed article titles.
+
+![](http://www.userlogos.org/logo/jumpordie/02142011/habrahabrru)
 
 ## Requirements
 
--- setup.py
+++ setup.py
@@ -8,4 +8,23 @@ setup(
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     version=__version__,
+
+    description='Print 3 most popular nouns from Habr feed article titles.',
+    url='https://github.com/Grin941/habr_challenge',
+    licence='MIT',
+    author='Grinenko Alexander',
+    author_email='labamifi@gmail.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Utilities',
+    ],
 )
